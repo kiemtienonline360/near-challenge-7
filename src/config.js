fb6a09b549fc5074649a4434124d028bcb03de74
Martin Tale <me@martintale.com>
2021-11-27 23:23:12 +0000

    contract name update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || "near-challenge-7.testnet";
+const CONTRACT_NAME = process.env.CONTRACT_NAME || "near-challenge-8.testnet";
 
 function getConfig(env) {
 	switch (env) {
